48b3dda2d73589c92e2920803b4255af29a5e9ab
Samet SELKİ <sametselki@outlook.com>
2023-11-25 21:54:00 +0300

    post itemlerin arkaplan rengi değiştirildi

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,13 +25,15 @@ export default function Home({ allPostsData }) {
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
+            <Link href={`/posts/${id}`} key={id}>
+              <li className={utilStyles.listItem}>
+                {title}
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            </Link>
           ))}
         </ul>
       </section>
